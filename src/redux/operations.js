2792e2f65a8ba2dbf863573c0b042a655b169f55
MartaShchur <m.matseyko199@gmail.com>
2023-11-15 18:40:48 +0100

    fix

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://6550c4077d203ab6626e2163.mockapi.io';
+// axios.defaults.baseURL = 'https://6550c4077d203ab6626e2163.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
